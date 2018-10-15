0058b9ef5ab5821817cbdcfae1620c09426ceaa0
Wallace Fontes <lalafontes78@gmail.com>
2018-10-15 17:35:03 -0400

    ajustes gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -21,16 +21,12 @@ elixir((mix) => {
 	/* JS Libs Site */
 	mix.scripts([
 		'resources/assets/js/site/libs/jquery-ui.js',
-		'resources/assets/js/site/libs/form-scripts.js',
-		'resources/assets/js/site/libs/validator.min.js',
-		'resources/assets/js/site/libs/jquery.mask.js',
 		], 'public/js/site/site-home-libs.js'); 
 
 	/* JS Site */
 	mix.scripts([
 		'resources/assets/js/site/modules/home.js',
 		], 'public/js/site/site-home.js'); 
-
 
 
 
@@ -42,17 +38,11 @@ elixir((mix) => {
 	/* SCSS Cms */
 	mix.sass('resources/assets/sass/cms/style.scss', 'public/css/cms/style.css');
 	mix.sass(['resources/assets/sass/cms/pages/home.scss'], 'public/css/cms/cms-dashboard.css');
-	
+
 
 	/* JS Libs Cms */
 	mix.scripts([
 		'resources/assets/js/cms/libs/jquery/dist/jquery.min.js',
-		'resources/assets/js/cms/libs/popper.js/dist/umd/popper.min.js',
-		'resources/assets/js/cms/libs/bootstrap/bootstrap.min.js',
-		'resources/assets/js/cms/libs/PACE/pace.min.js',
-		'resources/assets/js/cms/libs/chart.js/dist/Chart.min.js',
-		'resources/assets/js/cms/libs/nicescroll/jquery.nicescroll.min.js',
-		'resources/assets/js/cms/libs/jquery-loading/dist/jquery.loading.min.js',
 		], 'public/js/cms/app-libs.js'); 
 
 	// ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~
