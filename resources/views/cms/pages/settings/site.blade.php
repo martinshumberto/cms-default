49360119a655969b0b0ff9492464763e10d4d89d
Wallace Fontes <lalafontes78@gmail.com>
2018-10-17 09:06:18 -0400

    ajustes

--- resources/views/cms/pages/settings/site.blade.php
+++ resources/views/cms/pages/settings/site.blade.php
@@ -2,16 +2,98 @@
 
 @section('content')	
 
+
 <!-- Breadcrumb -->
-<ol class="breadcrumb bc-colored bg-theme" id="breadcrumb">
-	<li class="breadcrumb-item ">
-		<a href="">Home</a>
-	</li>
-{{-- 	<li class="breadcrumb-item">
-		<a href="#">Layouts</a>
-	</li> --}}
-	<li class="breadcrumb-item active">Configurações do Site</li>
-</ol>
+<div class="page-breadcrumb">
+	<div class="row">
+		<div class="col-5 align-self-center">
+			<h4 class="page-title">Configurações do Site</h4>
+		</div>
+		<div class="col-7 align-self-center">
+			<div class="d-flex align-items-center justify-content-end">
+				<nav aria-label="breadcrumb">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item">
+							<a href="{!!route('cms-dashboard')!!}">Dashboard</a>
+						</li>
+						<li class="breadcrumb-item active" aria-current="page">Configurações</li>
+						<li class="breadcrumb-item active" aria-current="page">Site</li>
+					</ol>
+				</nav>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">		
+		<div class="col-12">
+			<div class="card">
+				<div class="card-body">
+					<h4 class="card-title">Configuraçoes do Site</h4>
+					<h6 class="card-subtitle"> Todas as configurações de SEO e tags de Busca</h6>
+					
+					<div class="col-sm-12 col-xs-12">
+						<form class="form-horizontal" method="post" autocomplete="off" action="{!!route('cms-settings-site-update')!!}">
+							{{csrf_field()}}
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text" id="basic-addon1"><i class="fa fa-pencil-square-o"></i></span>
+								</div>
+								<input type="text" class="maxlength-input form-control" data-plugin="maxlength" data-placement="bottom-right-inside" maxlength="65" name="title" value="{!!$site->title!!}">
+							</div>
+							<div class="input-group mb-3">
+								<textarea class="maxlength-textarea form-control" data-plugin="maxlength" data-placement="bottom-right-inside" maxlength="150"
+								rows="4" name="description" >{!!$site->description!!}</textarea>
+							</div>
+							<div class="input-group mb-3">
+								<textarea class="maxlength-textarea form-control" data-plugin="maxlength" data-placement="bottom-right-inside" maxlength="200"
+								rows="4" placeholder="Palavras Chave" name="keywords">{!!$site->keywords!!}</textarea>
+							</div>
+							<div class="row">
+								<div class="col-lg-6">
+									<div class="input-group mb-3">
+										<div class="input-group-prepend">
+											<span class="input-group-text" id="basic-addon1"><i class="fa fa-envelope" aria-hidden="true"></i></span>
+										</div>
+										<input class="form-control" size="16" type="text" name="email" value="{!!$site->email!!}" placeholder="Email do Sistema">
+									</div>
+								</div>
+								<div class="col-lg-6">
+									<div class="input-group mb-3">
+										<div class="input-group-prepend">
+											<span class="input-group-text" id="basic-addon1"><i class="fa fa-lock" aria-hidden="true"></i></span>
+										</div>
+										<input class="form-control" size="16" type="text" name="password_email"  value="{!!$site->password_email!!}" placeholder="Senha do Email">
+								</div>
+							</div>
+							
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text">$</span>
+								</div>
+								<input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+								<div class="input-group-append">
+									<span class="input-group-text">.00</span>
+								</div>
+							</div>
+							<div class="input-group">
+								<div class="input-group-prepend">
+									<span class="input-group-text">$</span>
+								</div>
+								<div class="input-group-prepend">
+									<span class="input-group-text">0.00</span>
+								</div>
+								<input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+							</div>
+							<!-- form-group -->
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <div class="container">
 	<!-- Alerts -->
